d718d47eb8f6de748d29ee0998949172e2616306
Andres <afsaa@utp.edu.co>
2018-04-04 17:07:40 -0500

    Elbow method

---
+++ elbow_method.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include <random>
+#include <list>
+#include <vector>
+#include <math.h>
+#include "boost/tuple/tuple.hpp"
+
+using namespace std;
+using boost::tuple;
+
+typedef vector< tuple<int,double> > tuple_list;
+
+double calc_Inertia(int num_actual_cluster, list<double> distances_to_centroid) {
+  double inertia;
+  double total_distance = 0;
+  for (size_t i = 0; i < num_actual_cluster; i++) {
+     double subtotal_distance = 0;
+     for (auto n : distances_to_centroid) {
+       subtotal_distance += n;
+     }
+     total_distance += subtotal_distance;
+  }
+  inertia = pow(total_distance, 2);
+  return inertia;
+}
+
+void addInertiaTuple(tuple_list inertia_clusters, int num_actual_cluster, double actual_cluster_inertia) {
+  inertia_clusters.push_back( tuple<int, double>(num_actual_cluster, actual_cluster_inertia));
+}
+
+int main() {
+  tuple_list inertia_clusters;
+  int num_cluster; //Number of the actual cluster
+  double cluster_inertia; //Inertia of the actual cluster
+
+
+  //To iterate on the tuple list -->
+  //   for (tuple_list::const_iterator i = tl.begin(); i != tl.end(); ++i) {
+  //       cout << "Age: " << i->get<0>() << endl;
+  //       cout << "Name: " << i->get<1>() << endl;
+  //   }
+}

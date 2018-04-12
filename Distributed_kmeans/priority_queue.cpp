31a58b82b633a6535b5baac607a9e969e1641b74
Andres Fernando Saa Montaño <afsaa@utp.edu.co>
2018-04-12 14:16:50 -0500

    k-means distribuido

---
+++ Distributed_kmeans/priority_queue.cpp
@@ -0,0 +1,37 @@
+// constructing priority queues
+#include <iostream>       // std::cout
+#include <queue>          // std::priority_queue
+#include <vector>         // std::vector
+#include <functional>     // std::greater
+
+class mycomparison
+{
+  bool reverse;
+public:
+  mycomparison(const bool& revparam=false)
+    {reverse=revparam;}
+  bool operator() (const int& lhs, const int&rhs) const
+  {
+    if (reverse) return (lhs>rhs);
+    else return (lhs<rhs);
+  }
+};
+
+int main ()
+{
+
+  // using mycomparison:
+  typedef std::priority_queue<int,std::vector<int>,mycomparison> mypq_type;
+
+  mypq_type fourth;                       // less-than comparison
+  mypq_type fifth (mycomparison(true));   // greater-than comparison
+
+  //Push some numbers on fourth
+  fifth.push(5);
+  fifth.push(1);
+  fifth.push(6);
+  fifth.push(2);
+
+  std::cout << "Fourth first number: " << fifth.top() <<'\n';
+  return 0;
+}

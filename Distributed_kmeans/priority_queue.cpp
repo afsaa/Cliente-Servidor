1aff2d66aa223249b6301df9ca96efcc3b5a19dc
Andres <afsaa@utp.edu.co>
2018-04-16 15:33:09 -0500

    Instrucciones para definir la prioridad en la cola de prioridad.

--- Distributed_kmeans/priority_queue.cpp
+++ Distributed_kmeans/priority_queue.cpp
@@ -4,6 +4,10 @@
 #include <vector>         // std::vector
 #include <functional>     // std::greater
 
+// Para calcular la prioridad necesito la suma en el k que se está calculando, la suma del k anterior y la suma del k siguiente. Entre la suma anterior y la suma presente habría una pendiente (p1) y entre la suma presente y la suma siguiente habría una pendiente (p2). La prioridad se define por la diferencia entre p1 y p2.
+
+using namespace std;
+
 class mycomparison
 {
   bool reverse;
@@ -27,11 +31,11 @@ int main ()
   mypq_type fifth (mycomparison(true));   // greater-than comparison
 
   //Push some numbers on fourth
-  fifth.push(5);
-  fifth.push(1);
-  fifth.push(6);
-  fifth.push(2);
+  fourth.push(5);
+  fourth.push(1);
+  fourth.push(6);
+  fourth.push(2);
 
-  std::cout << "Fourth first number: " << fifth.top() <<'\n';
+  cout << "Fourth first number: " << fourth.top() <<'\n';
   return 0;
 }

7f919845fc93f77aeb5d2751a0b076a77f06ccaf
Andres <afsaa@utp.edu.co>
2018-04-19 11:09:05 -0500

    Función para obtener el mejor k con su prioridad. Pendiente terminarla.

--- Distributed_kmeans/priority_queue.cpp
+++ Distributed_kmeans/priority_queue.cpp
@@ -1,8 +1,10 @@
-// constructing priority queues
-#include <iostream>       // std::cout
-#include <queue>          // std::priority_queue
-#include <vector>         // std::vector
-#include <functional>     // std::greater
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+#include <utility>
+#include <list>
+#include <iterator>
 
 // Para calcular la prioridad necesito la suma en el k que se está calculando, la suma del k anterior y la suma del k siguiente. Entre la suma anterior y la suma presente habría una pendiente (p1) y entre la suma presente y la suma siguiente habría una pendiente (p2). La prioridad se define por la diferencia entre p1 y p2 dependiendo del cambio de pendiente entre las 2.
 // Cada prioridad va acompañado  de su k correspondiente.
@@ -32,11 +34,51 @@ struct SSDCompare
 	}
 };
 
+// Get a k_ssd
+int getSSD(int k) {
+	list<int> ssd_list {30, 23, 20, 18, 15};
+	auto ssd_list_front = ssd_list.begin();
+	advance(ssd_list_front, k);
+	// cout << *ssd_list_front << '\n';
+	return *ssd_list_front;
+}
+
+// Get three (3) values of k and throw the best k with its priority.
+pair<int,int> getBestK(int prev_k, int actual_k, int next_k) {
+	double slope1 = 0; // Pendiente 1
+	double slope2 = 0; // Pendiente 2
+	double slope_change = 0; //(cambio de pendiente)
+	int prev_kssd = getSSD(prev_k);
+	int actual_kssd = getSSD(actual_k);
+	int next_kssd = getSSD(next_k);
+
+	//slope = (y2-y1)/(x2-x1)
+	slope1 = (prev_kssd - actual_kssd)/(prev_k - actual_k);
+	slope2 = (next_kssd - actual_kssd)/(next_k - actual_k);
+	slope_change = slope1 - slope2;
+
+	// // Condition to set the best k with its priority.
+	// if (/* condition */) {
+	// 	/* code */
+	// }
+	// else{
+	//
+	// }
+
+	pair <int,int> best_k;
+	best_k.first = 5; //priority
+	best_k.second = prev_k; //Value of k
+	int priority = best_k.first;
+	int k_value = best_k.second;
+	cout << "Priority: "<< priority << " Value of k: "<< k_value;
+	cout << endl;
+	return {priority, k_value};
+}
 
 int main ()
 {
 
-  SSD SSD1(2, 200); //(prioridad, numero de k)
+  SSD SSD1(2, 200); //(priority, value of k)
 	SSD SSD2(1, 30);
 	SSD SSD3(5, 150);
 	SSD SSD4(1, 10);
@@ -57,6 +99,8 @@ int main ()
 		cout << "priority " << t.priority << " k_number " << t.k_number << endl;
 		queue.pop();
 	}
+
+	getBestK(1,2,3);
 
 	return 0;
 }
